972612dd217e7553b2cbf978474fb5d4360af819
Ryan Rishi <rdrishi12@gmail.com>
2014-10-21 20:45:11 +0200

    put twitter in footer made footer.js to write footer so I don't have to change every. damn. time.

---
+++ js/footer.js
@@ -0,0 +1,19 @@
+document.write('<div class="container">');
+//	left
+document.write('<div class="col-md-4">');
+//	social
+document.write('<a href="http://www.linkedin.com/in/ryanrishi" target="_blank"><img src="ico/linkedin.png"></a>&nbsp;');
+document.write('<a href="http://www.github.com/ryan-rishi" target="_blank"><img src="ico/github.png"></a>&nbsp;');
+document.write('<a href="http://www.twitter.com/rdrishi12" target="_blank"><img src="ico/twitter.png"></a>&nbsp;');
+document.write('</div>')
+//	middle
+document.write('<div class="col-md-4" align="center">');
+document.write('<p>&copy; 2014 Ryan Rishi. All Rights Reserved.</p>');
+document.write('<p>Created using <a href="http://www.getbootstrap.com" target="_blank">Bootstrap</a>.</p>');
+document.write('</div>');
+// right
+document.write('<div class="col-md-4" align="right">');
+document.write('<a href="contact.html">Contact</a>');
+
+document.write('</div>');
+document.write('</div>');
