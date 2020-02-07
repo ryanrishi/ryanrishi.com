27d39fa1d3b798b412d87281a08a2861d129abfd
Ryan Rishi <ryan@ryanrishi.com>
2020-02-06 21:32:58 -0800

    setup webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // webpack folder’s entry js — excluded from jekll’s build process.
+  entry: "./webpack/entry.js",
+  output: {
+    // we’re going to put the generated file in the assets folder so jekyll will grab it.
+    // if using GitHub Pages, use the following:
+    // path: "assets/javascripts"
+    path: "src/assets/javascripts/",
+    filename: "bundle.js"
+  },
+  module: {
+  loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      loader: "babel", // "babel-loader" is also a legal name to reference
+      query: {
+        presets: ["react", "es2015"]
+      }
+    }
+    ]
+  }
+};
