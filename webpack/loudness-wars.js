3a6dbd83d6e17d86a1c483c3cf6fa5589cfd9afc
Ryan Rishi <ryan@ryanrishi.com>
2020-02-14 17:20:41 -0800

    redraw chart on resize

--- webpack/loudness-wars.js
+++ webpack/loudness-wars.js
@@ -3,22 +3,11 @@ import * as d3 from 'd3';
 const margin = {
   top: 20,
   right: 20,
-  bottom: 30,
-  left: 40
+  bottom: 20,
+  left: 20
 };
 
 const $chart = document.querySelector('#chart');
-
-const width = $chart.offsetWidth - margin.left - margin.right;
-const height = 800 - margin.top - margin.bottom;
-
-const chart = d3.select('#chart');
-
-const svg = chart.append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 const x = d3.scaleTime();
 const y = d3.scaleLinear();
@@ -26,18 +15,22 @@ const y = d3.scaleLinear();
 const xAxis = d3.axisBottom().scale(x).ticks(d3.timeYear.every(5));
 const yAxis = d3.axisLeft().scale(y);
 
-d3.csv('/files/loudness-wars.csv', function(row) {
-  return {
-    artist: row.Artist,
-    song: row['Song Title'],
-    rank: +row.Position,
-    loudness: +row.Loudness,
-    year: +row.Year,
-    releaseDate: new Date(+row.Year, 0, 1) // not the actual release date, just a Date with the year
-  };
-}).then(function(data) {
-  // get rid of 0s / undefined
-  data = data.filter(song => song.loudness);
+let svg;
+
+const draw = (data) => {
+  console.log('draw');
+  const width = $chart.offsetWidth - margin.left - margin.right;
+  const height = 800 - margin.top - margin.bottom;
+
+  const chart = d3.select('#chart');
+
+  svg = svg ? svg : chart.append('svg');
+  svg.selectAll('*').remove();  // ehhhhhh, it'd be better to update in place
+  svg
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
   const years = data.map(r => r.year);
   x.domain(d3.extent([new Date(Math.min(...years), 0, 1), new Date(Math.max(...years), 0, 1)]))
@@ -97,22 +90,69 @@ d3.csv('/files/loudness-wars.csv', function(row) {
     .transition()
       .duration(200)
       .style('opacity', 0.9);
-  }
+  };
 
-  const onMouseout = (d) => {
+  const onMouseout = () => {
     tooltip.transition()
       .duration(300)
       .style('opacity', 0);
-  }
+  };
 
   svg.selectAll('.song')
     .data(data)
     .enter()
       .append('circle')
       .attr('class', 'song')
-      .attr('r', 3.5)
+      .attr('r', 2)
       .attr('cx', d => x(d.releaseDate))
       .attr('cy', d => y(d.loudness))
       .on('mouseover', onMouseover)
       .on('mouseout', onMouseout);
+
+  // trendline
+  const loudnessByYear = {};
+  for (let song of data) {
+    if (!loudnessByYear[song.year]) {
+      loudnessByYear[song.year] = [];
+    }
+
+    loudnessByYear[song.year].push(song);
+  }
+
+  const line = d3.line()
+    .x(d => x(d.year))
+    .y(d => y(d.loudness));
+
+  const meanLoudnessByYear = [];
+  for (let year in loudnessByYear) {
+    meanLoudnessByYear.push({
+      year: new Date(year, 0, 1),
+      loudness: d3.mean(loudnessByYear[year].map(x => x.loudness))
+    });
+  }
+
+  svg.append('path')
+    .datum(meanLoudnessByYear)
+    .attr('class', 'line')
+    .attr('d', line);
+};
+
+d3.csv('/files/loudness-wars.csv', function(row) {
+  return {
+    artist: row.Artist,
+    song: row['Song Title'],
+    rank: +row.Position,
+    loudness: +row.Loudness,
+    year: +row.Year,
+    releaseDate: new Date(+row.Year, 0, 1) // not the actual release date, just a Date with the year
+  };
+}).then(function(data) {
+  // get rid of 0s / undefined
+  data = data.filter(song => song.loudness);
+
+   draw(data);
+
+   window.addEventListener('resize', () => {
+     draw(data);
+   });
 });
