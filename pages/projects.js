b95dce3a45f0e42b9370f5a6312fbcbfebf99581
Ryan Rishi <ryan@ryanrishi.com>
2020-11-16 20:26:10 -0800

    add projects page

---
+++ pages/projects.js
@@ -0,0 +1,12 @@
+import ReactPlaceholder from "react-placeholder";
+import Layout from "../components/layout";
+import { H1, H3 } from "../components/headings";
+
+export default function Music() {
+  return (
+    <Layout>
+      <H1>Projects</H1>
+      <ReactPlaceholder type="text" rows={7} ready={false} />
+    </Layout>
+  );
+}
