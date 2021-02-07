477f5755cd9ba9ed21f92cc1b98652666e7559b7
Ryan Rishi <ryan@ryanrishi.com>
2021-02-07 11:05:31 -0700

    redirect Jekyll-style blog to Next-style blog
    /2020/01/17/tufte.html → /blog/2020-01-17-tufte
    https://github.com/vercel/next.js/issues/15712

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ const withMdxEnhanced = require('next-mdx-enhanced');
 module.exports = withMdxEnhanced({
   defaultLayout: true,
   fileExtensions: ['md', 'mdx']
-})();
+})({
+  async redirects() {
+    return [
+      {
+        // TODO redirect Jekyll to Next
+        // /2020/01/17/tufte.html → /blog/2020-01-17-tufte
+        // https://github.com/vercel/next.js/issues/15712
+        source: '/:year((?!_next).*)/:month/:day/:post.html',
+        destination: '/blog/:year-:month-:day-:post',
+        permanent: false
+      }
+    ]
+  }
+});
