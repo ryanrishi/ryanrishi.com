e01cc34b59a62e983990d2f6959a2596791d7b77
Ryan Rishi <ryan@ryanrishi.com>
2021-02-20 10:57:35 -0700

    add first Cypress test

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+context('Home', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('should find the title of the homepage', () => {
+    cy.get('h1').contains("Hi, I'm Ryan");
+  });
+});
