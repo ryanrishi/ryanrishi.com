5584d688bb15d790a909f88260116df87a4d9267
Ryan Rishi <ryan@ryanrishi.com>
2021-02-20 13:11:25 -0700

    add redirects spec

---
+++ cypress/integration/redirects.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+describe('Follows redirects from previous blog format', () => {
+  it('Managing the Same AWS Key Pair in Multiple Terraform Workspaces', () => {
+    cy.visit('/2020/06/01/managing-the-same-aws-key-pair-in-multiple-terraform-workspaces.html');
+    cy.get('h1').contains('Managing the Same AWS Key Pair in Multiple Terraform Workspaces');
+  });
+
+  it('Edward Tufte: Presenting Data and Information', () => {
+    cy.visit('2020/01/17/tufte.html');
+    cy.get('h1').contains('Edward Tufte: Presenting Data and Information');
+  });
+
+  it('Migrating My Website to AWS Using Terraform', () => {
+    cy.visit('2020/01/06/migrating-my-website-to-aws-using-terraform.html');
+    cy.get('h1').contains('Migrating My Website to AWS Using Terraform');
+  });
+
+  it('Migrating My Website to AWS Using Terraform', () => {
+    cy.visit('2020/01/06/migrating-my-website-to-aws-using-terraform.html');
+    cy.get('h1').contains('Migrating My Website to AWS Using Terraform');
+  });
+
+  it('The Twelve Days of an a cappella Christmas Album', () => {
+    cy.visit('2015/12/27/the-twelve-days-of-an-a-cappella-christmas-album.html');
+    cy.get('h1').contains('The Twelve Days of an a cappella Christmas Album');
+  });
+});
