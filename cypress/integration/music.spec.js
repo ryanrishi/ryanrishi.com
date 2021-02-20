04d5e55101f154d8e72a2d78784d088c52820028
Ryan Rishi <ryan@ryanrishi.com>
2021-02-20 12:49:49 -0700

    add music spec

---
+++ cypress/integration/music.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('Music', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/music');
+  });
+
+  it('should find the title of the music page', () => {
+    cy.get('h1').contains('Music');
+    cy.percySnapshot();
+  });
+
+  it('Senior Recital', () => {
+    cy.get('h3').contains('Senior Recital').scrollIntoView();
+    cy.percySnapshot();
+  });
+
+  it('Percussion Covers', () => {
+    cy.get('h3').contains('Percussion Covers').scrollIntoView();
+    cy.percySnapshot();
+  });
+
+  it('Recording', () => {
+    cy.get('h3').contains('Recording').scrollIntoView();
+    cy.percySnapshot();
+  });
+});
