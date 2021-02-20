87792fd879b9f2096d76c4d6b124ac999420cfcd
Ryan Rishi <ryan@ryanrishi.com>
2021-02-20 12:16:41 -0700

    add Cypress tests for test page

---
+++ cypress/integration/test_page.spec.js
@@ -0,0 +1,67 @@
+/// <reference types="cypress" />
+
+describe('Test Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/_test');
+  });
+
+  describe('Title + Date', () => {
+    it('Title + Date', () => {
+      const title = cy.get('h1').contains('test');
+      title.next().should('have.text', 'February 2, 2021'); // .should((date) => expect(date.text()).to.equal('February 2, 2021'));
+      cy.percySnapshot();
+    });
+  })
+
+  describe('Viewport Tests', () => {
+    beforeEach(() => {
+      const screenSizesSection = cy.get('h1').contains('Screen Sizes');
+      screenSizesSection.scrollIntoView();
+    });
+
+    it('viewport - < sm', () => {
+      cy.viewport(575, 1000);
+      cy.get('.sm\\:hidden').should('be.visible');
+      cy.get('.md\\:hidden').should('be.visible');
+      cy.get('.lg\\:hidden').should('be.visible');
+      cy.get('.xl\\:hidden').should('be.visible');
+      cy.percySnapshot();
+    });
+
+    it('viewport - sm to md', () => {
+      cy.viewport(767, 1000);
+      cy.get('.sm\\:hidden').should('not.be.visible');
+      cy.get('.md\\:hidden').should('be.visible');
+      cy.get('.lg\\:hidden').should('be.visible');
+      cy.get('.xl\\:hidden').should('be.visible');
+      cy.percySnapshot();
+    });
+
+    it('viewport - md to lg', () => {
+      cy.viewport(991, 1000);
+      cy.get('.sm\\:hidden').should('not.be.visible');
+      cy.get('.md\\:hidden').should('not.be.visible');
+      cy.get('.lg\\:hidden').should('be.visible');
+      cy.get('.xl\\:hidden').should('be.visible');
+      cy.percySnapshot();
+    });
+
+    it('viewport - lg to xl', () => {
+      cy.viewport(1199, 1000);
+      cy.get('.sm\\:hidden').should('not.be.visible');
+      cy.get('.md\\:hidden').should('not.be.visible');
+      cy.get('.lg\\:hidden').should('not.be.visible');
+      cy.get('.xl\\:hidden').should('be.visible');
+      cy.percySnapshot();
+    });
+
+    it('viewport - > xl', () => {
+      cy.viewport(1200, 1000);
+      cy.get('.sm\\:hidden').should('not.be.visible');
+      cy.get('.md\\:hidden').should('not.be.visible');
+      cy.get('.lg\\:hidden').should('not.be.visible');
+      cy.get('.xl\\:hidden').should('not.be.visible');
+      cy.percySnapshot();
+    });
+  });
+});
