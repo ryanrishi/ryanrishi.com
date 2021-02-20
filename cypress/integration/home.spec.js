dfe0a4e6421e2cc87cbc65c9d6de8fb83a5b8fb4
Ryan Rishi <ryan@ryanrishi.com>
2021-02-20 11:16:02 -0700

    add Percy to Cypress
    https://docs.percy.io/docs/cypress

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,8 +1,12 @@
 /// <reference types="cypress" />
 
-context('Home', () => {
+describe('Home', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
+  });
+
+  it('percy', () => {
+    cy.percySnapshot();
   });
 
   it('should find the title of the homepage', () => {
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+const percyHealthCheck = require('@percy/cypress/task');
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -16,6 +17,7 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
+  on('task', percyHealthCheck);
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
