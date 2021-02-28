8397ebd7d95d9caa1cdde157e6414f0bf95e74ef
Ryan Rishi <ryan@ryanrishi.com>
2021-02-28 11:54:33 -0700

    pass image from project to head

--- layouts/projects/default.js
+++ layouts/projects/default.js
@@ -2,13 +2,14 @@ import Head from '../../components/head';
 import Layout from '../../components/layout';
 
 export default function Index({ children, frontMatter }) {
-  const { name, blurb } = frontMatter;
+  const { name, blurb, image } = frontMatter;
 
   return (
     <Layout>
       <Head
         title={name}
         description={blurb}
+        image={image}
       />
       <div>
         <h1>{name}</h1>
