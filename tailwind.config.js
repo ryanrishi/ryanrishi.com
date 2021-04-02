81fac2cf17fd26aa4b6fcd67298b87ac6c788867
Ryan Rishi <ryan@ryanrishi.com>
2021-04-01 18:48:22 -0700

    make ESLint happy

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,21 @@ module.exports = {
     container: {
       center: true
     },
-    extend: {},
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
   plugins: [
-    plugin(function({ addBase, theme }) {
+    plugin(({ addBase, theme }) => {
       addBase({
-        h1: { fontSize: theme('fontSize.4xl')},
-        h2: { fontSize: theme('fontSize.3xl')},
-        h3: { fontSize: theme('fontSize.2xl')},
-        h4: { fontSize: theme('fontSize.xl')},
-        h5: { fontSize: theme('fontSize.lg')},
-        h6: { fontSize: theme('fontSize.xs')},
-      })
+        h1: { fontSize: theme('fontSize.4xl') },
+        h2: { fontSize: theme('fontSize.3xl') },
+        h3: { fontSize: theme('fontSize.2xl') },
+        h4: { fontSize: theme('fontSize.xl') },
+        h5: { fontSize: theme('fontSize.lg') },
+        h6: { fontSize: theme('fontSize.xs') }
+      });
     })
-  ],
-}
+  ]
+};
