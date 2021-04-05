998527126f5e02e57e5c9c8ef6678c24711afbdd
Ryan Rishi <ryan@ryanrishi.com>
2021-04-05 09:11:05 -0700

    make video container its own component

---
+++ components/video-container.js
@@ -0,0 +1,17 @@
+export default function VideoContainer({ src, title }) {
+  return (
+    <div
+      className="relative pt-8"
+      style={{ paddingBottom: '50%' }}
+    >
+      <iframe
+        src={src}
+        title={title}
+        className="absolute w-full h-full top-0 pt-4"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
+  );
+}
--- pages/music.js
+++ pages/music.js
@@ -4,47 +4,39 @@
 import Link from '../components/link';
 import Layout from '../components/layout';
 import Head from '../components/head';
+import { H1, H3 } from '../components/headings';
+import VideoContainer from '../components/video-container';
 
 export default function Music() {
   return (
     <Layout>
       <Head title="Music" />
-      <h1 className="mt-0">Music</h1>
+      <H1 className="mt-0">Music</H1>
 
-      <h3>Senior Recital</h3>
+      <H3>Senior Recital</H3>
       <p>
-        As a music major, I was awarded the opportunity to put on a recital in my senior year. It composed of various musical elements including live looping marimba with <a href="https://www.ableton.com/">Ableton Live</a>, percussion ensemble, <em>a cappella</em>, gamelan, and mallet solos.
+        As a music major, I was awarded the opportunity to put on a recital in my senior year. It composed of various musical elements including live looping marimba with <Link href="https://www.ableton.com/">Ableton Live</Link>, percussion ensemble, <em>a cappella</em>, gamelan, and mallet solos.
       </p>
-      <div className="video-container">
-        <iframe
-          src="https://www.youtube-nocookie.com/embed/QEQ5AZ_LAlQ"
-          title='Ryan Rishi - "8 on Three and 9 on Two"'
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      </div>
+      <VideoContainer
+        src="https://www.youtube-nocookie.com/embed/QEQ5AZ_LAlQ"
+        title='Ryan Rishi - "8 on Three and 9 on Two"'
+      />
 
-      <h3>Percussion Covers</h3>
+      <H3>Percussion Covers</H3>
       <p>In college, I enjoyed arranging and recording popular songs played on keyboard percussion instruments.</p>
 
-      <div className="video-container">
-        <iframe
-          src="https://www.youtube-nocookie.com/embed/sMzCV91D3ao"
-          title='Ryan Rishi - "I Will Follow You Into The Dark" by Death Cab for Cutie [Percussion Cover]'
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      </div>
+      <VideoContainer
+        src="https://www.youtube-nocookie.com/embed/sMzCV91D3ao"
+        title='Ryan Rishi - "I Will Follow You Into The Dark" by Death Cab for Cutie [Percussion Cover]'
+      />
 
-      <h3>Recording</h3>
+      <H3>Recording</H3>
       <p>
         I've produced two albums.
         The first is Christmas In Claradise, which was a joint effort with the five <em>a cappella</em> groups at Santa Clara University. You can read about it <Link href="/blog/2015-12-27-the-twelve-days-of-an-a-cappella-christmas-album">here</Link>.
       </p>
       <p>
-        The second is <a href="https://open.spotify.com/album/5Tbpm70sMQ2aUwdsEieHn5?si=pucr00KFSq-X_hMGfvNRrA">Bellomy</a>, an album of songs written by my band from college.
+        The second is <Link href="https://open.spotify.com/album/5Tbpm70sMQ2aUwdsEieHn5?si=pucr00KFSq-X_hMGfvNRrA">Bellomy</Link>, an album of songs written by my band from college.
       </p>
 
       <iframe
