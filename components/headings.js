6e64687c84bef1c7a69110cfe76565ed33acb7e1
Ryan Rishi <ryan@ryanrishi.com>
2021-04-10 23:36:58 -0700

    cleanup headings

--- components/headings.js
+++ components/headings.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 
-const baseClassNames = 'italic uppercase font-extrabold text-gray-800 mb-16 mt-8';
+const baseClassNames = 'italic uppercase font-extrabold text-gray-800 mb-4 lg:mb-8 mt-4 lg:mt-6';
 
 export function H1({ className, children }) {
   const classes = classNames(baseClassNames, className);
