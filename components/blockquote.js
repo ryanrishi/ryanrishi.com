8fe4e7c4c5f781501fd8d4070b021c3e0ec3609e
Ryan Rishi <ryan@ryanrishi.com>
2021-05-05 20:06:38 -0700

    fix ESLint long line

--- components/blockquote.js
+++ components/blockquote.js
@@ -5,7 +5,10 @@ import Link from './link';
 export default function Quote({ children, name, citationLink }) {
   return (
     <blockquote className="relative p-4 text-xl text-gray-600 quote italic mb-4 border-l-4 sm:border-l-0">
-      <div className="text-gray-300 text-8xl italic right-full absolute top-0 leading-none hidden sm:block font-serif" aria-hidden="true">
+      <div
+        className="text-gray-300 text-8xl italic right-full absolute top-0 leading-none hidden sm:block font-serif"
+        aria-hidden="true"
+      >
         &ldquo;
       </div>
       <p className="mb-4">{children}</p>
