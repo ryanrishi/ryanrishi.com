6024a7a71a98d1c88f506c5808dccdde3acb3b1f
Ryan Rishi <ryan@ryanrishi.com>
2021-05-05 21:20:22 -0700

    add social icons to footer

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,46 @@
+import { ImGithub, ImLinkedin, ImSoundcloud, ImYoutube } from 'react-icons/im';
+import Link from './link';
+
+const FooterSocialLink = ({ children, href }) => (
+  <Link
+    href={href}
+    className="px-4 text-2xl"
+    invert
+  >
+    {children}
+  </Link>
+);
+
 export default function Footer() {
   return (
-    <footer className="container uppercase italic font-bold text-sm px-4 my-16">
+    <footer className="flex flex-col md:flex-row justify-between container uppercase italic font-bold text-sm px-4 my-8 md:my-16">
       <div>
         <p>
           {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
           &copy; Copyright&nbsp;{ new Date().getFullYear() }&nbsp;Ryan Rishi
         </p>
+      </div>
+      <div className="flex flex-row justify-center pt-4 md:pt-0">
+        <FooterSocialLink
+          href="https://github.com/ryanrishi"
+        >
+          <ImGithub />
+        </FooterSocialLink>
+        <FooterSocialLink
+          href="https://linkedin.com/in/ryanrishi"
+        >
+          <ImLinkedin />
+        </FooterSocialLink>
+        <FooterSocialLink
+          href="https://soundcloud.com/ryanrishi"
+        >
+          <ImSoundcloud />
+        </FooterSocialLink>
+        <FooterSocialLink
+          href="https://youtube.com/RyanRishiPercussion"
+        >
+          <ImYoutube />
+        </FooterSocialLink>
       </div>
     </footer>
   );
