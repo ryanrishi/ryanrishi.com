6d48161cc65edf30f596d95285c02f888d5d92e7
Ryan Rishi <ryan@ryanrishi.com>
2021-06-04 17:54:59 -0600

    rm bottom margin from name in contact form

--- pages/contact.js
+++ pages/contact.js
@@ -16,7 +16,7 @@ export default function Contact() {
           method="POST"
         >
           <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3 mb-6 md:mb-0">
+            <div className="w-full px-3">
               <label
                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 htmlFor="grid-full-name"
