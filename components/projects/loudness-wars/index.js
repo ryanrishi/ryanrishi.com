272474964d8b25c16f7256ccd550db8d7f29c58b
Ryan Rishi <ryan@ryanrishi.com>
2021-06-28 18:15:46 -0600

    get d3 working

--- components/projects/loudness-wars/index.js
+++ components/projects/loudness-wars/index.js
@@ -2,14 +2,39 @@ import { useEffect, useRef } from 'react';
 
 // see https://stackoverflow.com/questions/65974337/import-es-module-in-next-js-err-require-esm
 // see https://github.com/d3/d3/issues/3469
-const d3 = import('d3').default;
+let d3;
 
-const drawChart = (svgRef) => {
-  debugger
-  console.log('d3', d3);
+// there has to be a better way to use a non-ESM module here
+const ensureD3 = async () => {
+  if (!d3) {
+    d3 = await import('d3');
+  }
+};
+
+const drawChart = async (svgRef) => {
+  await ensureD3();
   const svg = d3.select(svgRef.current);
 
-  svg.append('g');
+  const data = [12, 5, 6, 6, 9, 10];
+  const h = 120;
+  const w = 250;
+
+  svg
+    .attr("width", w)
+    .attr("height", h)
+    .style("margin-top", 50)
+    .style("margin-left", 50);
+
+  svg
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => i * 40)
+    .attr("y", (d, i) => h - 10 * d)
+    .attr("width", 20)
+    .attr("height", (d, i) => d * 10)
+    .attr("fill", "steelblue");
 };
 
 const Chart = () => {
