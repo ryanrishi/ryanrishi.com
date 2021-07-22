48a5baf57d7def9155c600aca984840981b4d1c2
Ryan Rishi <ryan@ryanrishi.com>
2021-07-21 23:31:19 -0600

    WIP animate projects index

--- pages/projects.js
+++ pages/projects.js
@@ -1,33 +1,52 @@
+import { animated, useSpringRef, useTransition } from 'react-spring';
 import Link from '../components/link';
 import Layout from '../components/layout';
 import Head from '../components/head';
-import { H1 } from '../components/headings';
+import { H1, H3 } from '../components/headings';
 import { getAllProjects } from '../lib/projects';
 
-const ProjectItem = ({ title, description, href }) => (
-  <li className="pb-2">
-    <Link href={href}>{title}</Link>
-    &mdash;
-    {' '}
+const ProjectItem = ({ title, description, href, style }) => (
+  <animated.div
+    className="border rounded p-4"
+    style={{ ...style }}
+  >
+    <H3>
+      <Link
+        href={href}
+      >
+        {title}
+      </Link>
+    </H3>
     {description}
-  </li>
+  </animated.div>
 );
 
 export default function ProjectsIndex({ projects }) {
+  const transApi = useSpringRef();
+  const transition = useTransition(projects, {
+    ref: transApi,
+    trail: 500 / projects.length,
+    from: { opacity: 0, scale: 0 },
+    enter: { opacity: 1, scale: 1 },
+    leave: { opacity: 0, scale: 0 }
+  });
+
   return (
     <Layout>
       <Head title="Projects" />
       <H1>Projects</H1>
-      <ul className="list-disc list-outside px-4">
-        {projects.map(project => (
+      <div
+        className="grid grid-cols-1 md:grid-cols-3 gap-4"
+      >
+        {transition((style, project) => (
           <ProjectItem
-            key={project.permalink}
-            href={project.permalink}
-            description={project.description}
             title={project.name}
+            description={project.description}
+            href={project.permalink}
+            style={style}
           />
         ))}
-      </ul>
+      </div>
     </Layout>
   );
 }
