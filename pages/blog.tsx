d7249f44355c130ae259cef7844b2a6e6120682b
Ryan Rishi <ryan@ryanrishi.com>
2022-01-02 13:10:01 -0700

    use next/link for blog index

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,12 +1,12 @@
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import { GetStaticProps } from 'next'
+import Link from 'next/link'
 import React from 'react'
 
 import Head from '../components/head'
 import { H2 } from '../components/headings'
 import Layout from '../components/layout'
-import Link from '../components/link'
 import { getAllPosts, Post } from '../lib/posts'
 
 interface BlogProps {
@@ -30,16 +30,19 @@ export default function BlogIndex({ posts }: BlogProps) {
           <H2 className="post-title">
             <Link
               href={`/blog/${post.slug}`}
-              invert
             >
               {post.title}
             </Link>
           </H2>
-          <p className="py-4 text-gray-700" data-test-blog-post-date>{dayjs.utc(post.date).format('MMMM D, YYYY')}</p>
+          <p className="pb-4 text-gray-700" data-test-blog-post-date>{dayjs.utc(post.date).format('MMMM D, YYYY')}</p>
 
           <div className="flex flex-col">
             <p>{post.description}</p>
-            <Link className="flex justify-end italic uppercase font-bold" href={`/blog/${post.slug}`} invert>Read more &raquo;</Link>
+            <Link href={`/blog/${post.slug}`}>
+              <a className="flex justify-end pt-4 italic uppercase font-bold">
+                Read more &raquo;
+              </a>
+            </Link>
           </div>
         </div>
       ))}
