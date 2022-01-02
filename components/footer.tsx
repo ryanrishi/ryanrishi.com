d4840bbe63cba4a93fa2560961527fef1963aac5
Ryan Rishi <ryan@ryanrishi.com>
2022-01-02 12:19:36 -0700

    add footer content
    might add some text to social links, eg. `:octocat: Github`

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,46 +1,76 @@
+import classNames from 'classnames'
+import NextLink from 'next/link'
+import { ReactNode } from 'react'
 import { ImGithub, ImLinkedin, ImSoundcloud, ImYoutube } from 'react-icons/im'
 
-import Link from './link'
+/*
+- dark gray background
+  - dark mode: same color as rest of page
+
+Content:
+2 columns:
+Home                      Github
+About                     LinkedIn
+Blog                      Twitter
+Projects                  Contact
+
+      (c) 2022 Ryan Rishi. All Right Reserved
+      Unless explicitly expressed, all views are my own.
+
+ */
+
+interface FooterLinkProps {
+  children: ReactNode;
+  href: string;
+  className?: string;
+}
+
+const FooterLink = ({ children, href, className }: FooterLinkProps) => {
+  const classes = classNames('flex flex-row hover:text-gray-600 transition py-2', className)
+
+  return (
+    <NextLink href={href}>
+      <a className={classes}>{children}</a>
+    </NextLink>
+  )
+}
 
 const FooterSocialLink = ({ children, href }) => (
-  <Link
+  <FooterLink
     href={href}
-    className="px-4 text-2xl"
-    invert
+    className="space-x-2"
   >
     {children}
-  </Link>
+  </FooterLink>
 )
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col md:flex-row justify-between container uppercase italic font-bold text-sm px-4 my-8 md:my-16">
-      <div>
-        <p>
-          &copy; Copyright&nbsp;{ new Date().getFullYear() }&nbsp;Ryan Rishi
-        </p>
+    <footer className="bg-gray-900 text-gray-500">
+      <div className="container max-w-4xl p-4 lg:p-8 flex flex-row">
+        <div className="flex flex-col w-1/2">
+          <FooterLink href="/">Home</FooterLink>
+          <FooterLink href="/about">About</FooterLink>
+          <FooterLink href="/blog">Blog</FooterLink>
+          <FooterLink href="/projects">Projects</FooterLink>
+        </div>
+        <div className="flex flex-col w-1/2">
+          <FooterSocialLink href="https://github.com/ryanrishi">
+            <ImGithub />
+          </FooterSocialLink>
+          <FooterSocialLink href="https://linkedin.com/in/ryanrishi">
+            <ImLinkedin />
+          </FooterSocialLink>
+          <FooterSocialLink href="https://soundcloud.com/ryanrishi">
+            <ImSoundcloud />
+          </FooterSocialLink>
+          <FooterSocialLink href="https://youtube.com/RyanRishiPercussion">
+            <ImYoutube />
+          </FooterSocialLink>
+        </div>
       </div>
-      <div className="flex flex-row justify-center pt-4 md:pt-0">
-        <FooterSocialLink
-          href="https://github.com/ryanrishi"
-        >
-          <ImGithub />
-        </FooterSocialLink>
-        <FooterSocialLink
-          href="https://linkedin.com/in/ryanrishi"
-        >
-          <ImLinkedin />
-        </FooterSocialLink>
-        <FooterSocialLink
-          href="https://soundcloud.com/ryanrishi"
-        >
-          <ImSoundcloud />
-        </FooterSocialLink>
-        <FooterSocialLink
-          href="https://youtube.com/RyanRishiPercussion"
-        >
-          <ImYoutube />
-        </FooterSocialLink>
+      <div className="container flex justify-center align-middle py-4">
+        <p>&copy; Copyright&nbsp;{ new Date().getFullYear() }&nbsp;Ryan Rishi</p>
       </div>
     </footer>
   )
