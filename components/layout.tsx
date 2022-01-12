41a064a303d13e9dbe028e76276b9bbb35a8013e
Ryan Rishi <ryan@ryanrishi.com>
2022-01-11 18:07:33 -0700

    make light mode a little less light

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import Header from './header'
 
 export default function Layout({ children }) {
   return (
-    <div className="dark:bg-gray-900 transition">
+    <div className="bg-gray-50 dark:bg-gray-900 transition">
       <Header />
       <div className="container max-w-4xl p-4 lg:p-8 leading-8">
         {children}
