be30b1c0ced1ade778cbeace1c8febb134901515
Ryan Rishi <ryan@ryanrishi.com>
2023-01-22 12:22:34 -0800

    [skip ci] Use email for ACM validation (#110)

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "certificate" {
   domain_name               = var.root_domain_name
-  validation_method         = "EMAIL"
+  validation_method         = "DNS"
   subject_alternative_names = ["*.${var.root_domain_name}"]
 
   lifecycle {
