9ce61e15df5b0f49984475ba7d323c0a2322ad23
Ryan Rishi <ryan@ryanrishi.com>
2023-02-18 16:52:11 -0500

    change logo color in dark mode

--- components/header.tsx
+++ components/header.tsx
@@ -122,8 +122,8 @@ export default function Header() {
         />
       </div>
       <nav className="hidden md:flex flex-row justify-between items-center w-full">
-        <div className="flex flex-row items-center">
           <Logo />
+        <div className="flex flex-row items-center dark:text-green-200">
           <HeaderLink href="/" className="ml-4">Ryan Rishi</HeaderLink>
         </div>
         <div className="flex flex-row items-center">
