29aedd2a5e4b3e4f8b5535e36cc9acee7135a32e
Ryan Rishi <ryan@ryanrishi.com>
2023-11-13 16:25:33 -0500

    migrate 404

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next/types'
+
+import Link from '../components/link'
+
+export const metadata: Metadata = {
+  title: '404 | Ryan Rishi',
+}
+
+export default function Error404() {
+  return <p>The page you are looking for does not exist. <Link href="/">Return home</Link>?</p>
+}
