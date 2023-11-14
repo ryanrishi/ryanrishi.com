cfc5c543920c0e09b2f2889e030ab2c09ce10ed2
Ryan Rishi <ryan@ryanrishi.com>
2023-11-13 21:16:28 -0500

    pull out project item into its own file since I'll maybe use client at some point

---
+++ app/projects/project-item.tsx
@@ -0,0 +1,30 @@
+import { Project } from 'contentlayer/generated'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function ProjectItem({ project }: { project: Project }) {
+  return (
+    <li className="border border-slate-200 dark:border-slate-700 rounded shadow">
+      <div className="flex items-center justify-center">
+        <Link href={project.url}>
+          <Image
+            className="object-cover transition duration-500 ease-in-out transform hover:scale-105 max-w-full h-auto rounded-t"
+            src={project.image.src || 'https://via.placeholder.com/400'}
+            alt={project.image.alt}
+            width="1600"
+            height="1600"
+            objectFit="cover"
+          />
+        </Link>
+      </div>
+      <div className="p-4">
+        <H3>
+          <Link href={project.url}>
+            {project.name}
+          </Link>
+        </H3>
+        <p className="transition">{project.description}</p>
+      </div>
+    </li>
+  )
+}
