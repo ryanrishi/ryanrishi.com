9aa64d09b36a2a2635288157c7ac65b974d8996b
Ryan Rishi <ryan@ryanrishi.com>
2023-11-14 22:01:17 -0500

    get cypress in a runnable state

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,18 +38,6 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  // const router = useRouter()
-
-  // useEffect(() => {
-  //   // When the component is mounted, subscribe to router changes and log those page views
-  //   router.events.on('routeChangeComplete', url => pageview(url))
-
-  //   // If the component is unmounted, unsubscribe from the event with the `off` method
-  //   return () => {
-  //     router.events.off('routeChangeComplete', () => pageview())
-  //   }
-  // }, [router.events])
-
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from 'next/types'
 
 import Link from '../components/link'
+import { H1 } from '@/components/headings'
 
 export const metadata: Metadata = {
   title: '404',
 }
 
 export default function Error404() {
-  return <p>The page you are looking for does not exist. <Link href="/">Return home</Link>?</p>
+  return (
+    <>
+      <H1>404</H1>
+      <p>The page you are looking for does not exist. <Link href="/">Return home</Link>?</p>
+    </>
+  )
 }
