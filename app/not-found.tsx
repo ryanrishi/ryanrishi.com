ce11e5e7cfcfede481baf469a6027810cdfb4345
Ryan Rishi <ryan@ryanrishi.com>
2023-11-17 11:52:15 -0500

    use FancyH1 for 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next/types'
 
-import { H1 } from '@/components/headings'
+import { FancyH1 } from '@/components/headings'
 
-import Link from '../components/link'
+import Link from '@/components/link'
 
 export const metadata: Metadata = {
   title: '404',
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function Error404() {
   return (
     <>
-      <H1>404</H1>
+      <FancyH1>404</FancyH1>
       <p>The page you are looking for does not exist. <Link href="/">Return home</Link>?</p>
     </>
   )
