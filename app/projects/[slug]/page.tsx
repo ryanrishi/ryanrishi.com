7d8b74da372d9610746e70f9ff9965b4c7647804
Ryan Rishi <ryan@ryanrishi.com>
2023-11-20 20:56:32 -0500

    fix project image link in head

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -24,7 +24,7 @@ export const generateMetadata = ({ params }: { params: { slug: string } }): Meta
       publishedTime: project.date,
       url: `https://ryanrishi.com/projects/${decodeURIComponent(params.slug)}`,
       images: [
-        { url: `https://ryanrishi.com/${project.image}`},
+        { url: `https://ryanrishi.com/${project.image.src}`},
       ],
     },
   }
