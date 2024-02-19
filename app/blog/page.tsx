40ef0e89a25e9a49abc0052bd4b77db9da751d24
Ryan Rishi <ryan@ryanrishi.com>
2024-02-19 14:33:23 -0500

    prosify blog index

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -25,27 +25,22 @@ export default function BlogIndex() {
   return (
     <>
       <FancyH1>Blog</FancyH1>
-      {posts.map((post, i) => (
-        <div
-          className="mb-16 lg:mb-16 xl:mb-32"
-          data-test-blog-post
-          key={i}
-        >
-          <H1 className="hover:text-slate-900 dark:hover:text-slate-300">
-            <Link href={`blog/${post.slug}`} legacyBehavior>
-              {post.title}
-            </Link>
-          </H1>
-          <p className="pb-4 text-slate-700 dark:text-slate-400 transition" data-test-blog-post-date>{dayjs.utc(post.publishedAt).format('MMMM D, YYYY')}</p>
-
-          <div className="flex flex-col">
+      <div className="prose dark:prose-invert">
+        {posts.map((post, i) => (
+          <div
+            key={i}
+            data-test-blog-post
+          >
+            <h2>
+              <Link href={`blog/${post.slug}`}>
+                {post.title}
+              </Link>
+            </h2>
+            <p className="-mt-4 text-slate-500 transition" data-test-blog-post-date>{dayjs.utc(post.publishedAt).format('MMMM D, YYYY')}</p>
             <p className="transition">{post.description}</p>
-            <Link className="flex justify-end pt-4 italic uppercase font-bold dark:text-slate-100 transition" href={`blog/${post.slug}`}>
-              Read more &raquo;
-            </Link>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </>
   )
 }
