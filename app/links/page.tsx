f3add6c4db9f8d337f5f851b61c3286dbd1bd96a
Ryan Rishi <ryan@ryanrishi.com>
2024-06-23 10:51:25 -0400

    clean up links page heading

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -81,7 +81,7 @@ export default function Links() {
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="flex flex-col items-center justify-center w-[75vw] max-w-screen-md">
         <Image className="rounded-full h-32 w-32" src={headshot} alt="Ryan Rishi" />
-        <h1>Ryan Rishi</h1>
+        <h1 className="text-4xl mt-4 mb-8">Ryan Rishi</h1>
         <p className="text-center">Experienced software engineer with expertise in backend, infra, and AI</p>
         <br />
         <div className="flex items-center justify-center">
